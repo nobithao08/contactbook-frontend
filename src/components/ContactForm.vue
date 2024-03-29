fcd6393ab318ca7f3c10069f1cab80653412f052
buithao <thaob2014700@student.ctu.edu.vn>
2024-03-29 09:42:10 +0700

    Tao trang them lien he

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -57,13 +57,15 @@
         </div>
 
         <div class="form-group">
-            <button class="btn btn-primary">Lưu</button>
+            <button class="btn btn-primary">
+                <i class="fas fa-save"></i> Lưu</button>
             <button
                 v-if="contactLocal._id"
                 type="button"
                 class="ml-2 btn btn-danger"
                 @click="deleteContact"
             >
+            <i class="fas fa-trash-alt"></i>
                 Xóa
             </button>
         </div>
